a82e51f6d68df1161d6b80d1bc7ab3fdc565f802
dannymayer <dannyrmayer@gmail.com>
2021-01-15 14:00:52 -0600

    Connect DB

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 
+# /// = relative path, //// = absolute path
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    todo_list = Todo.query.all()
+    return render_template('base.html', todo_list=todo_list)
+
 
 if __name__ == "__main__":
-  app.run(debug=True)
+    db.create_all()
+    app.run(debug=True)
